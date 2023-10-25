21341e168ba8c8a22521292957c0533e4b818dcc
Isaac Malin <Isaacmalin3530@gmail.com>
2023-10-24 18:45:04 -0600

    completed guided project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import Title from './components/Title';
-import DragonList from './components/DragonList';
-import './styles.css';
+import Title from "./components/Title";
+import DragonList from "./components/DragonList";
+import "./styles.css";
+import { legacy_createStore as createStore } from "redux";
+import { rootReducer } from "./reducers";
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 function App() {
   return (
@@ -14,5 +19,10 @@ function App() {
   );
 }
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
